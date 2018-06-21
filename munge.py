a116600fef472305dba7d7588560be9b9b2f84a4
Robin Linacre <robinlinacre@hotmail.com>
2018-06-21 09:32:55 +0100

    Update munge.py

--- munge.py
+++ munge.py
@@ -2,7 +2,6 @@
 import pandas as pd 
 
 df = pd.read_csv("https://www.gov.uk/government/uploads/system/uploads/attachment_data/file/695412/cc-rdos-by-offence-group-q4-2017.csv")
-
 # Delete totals - these should not be present in tidy data
 drop = [c for c in df.columns if 'Total' in c]
 df = df.drop(drop, axis=1)
@@ -15,6 +14,7 @@ id_cols = [c for c in df.columns if '-' not in c]
 value_cols = [c for c in df.columns if '-' in c]
 
 df = pd.melt(df,id_vars = id_cols , value_vars = value_cols)
+
 
 renames = {
     "quarter.1" : "date_range",
@@ -43,5 +43,14 @@ df = df[cols]
 
 df["value"] = pd.to_numeric(df["value"], errors='coerce')
 
-df.to_csv("timeliness-transparency-2017Q4.csv")
+ind = ['variable_type']
+ind.extend([c for c in df.columns if c not in ('variable_type', 'value')])
+
+df = df.set_index(ind).unstack(level=0).reset_index()
+df.columns = [' '.join(col).strip() for col in df.columns.values] 
+df.columns = [df.columns.str.replace("value ", "").str.lower()]
+
+
+
+df.to_csv("timeliness-transparency-2017Q4.csv" index=False)
 
